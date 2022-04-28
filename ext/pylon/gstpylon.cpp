68093794a7926fbb8a5d12702909a85bda776961
Marisol Zeledon <marisol.zeledon@ridgerun.com>
2022-04-28 13:13:48 -0600

    Indent code following gst indentation standard

--- ext/pylon/gstpylon.cpp
+++ ext/pylon/gstpylon.cpp
@@ -11,48 +11,67 @@
 #include <pylon/PylonIncludes.h>
 #include <pylon/BaslerUniversalInstantCamera.h>
 
-struct _GstPylon {
-  Pylon::CBaslerUniversalInstantCamera *camera;
+struct _GstPylon
+{
+  Pylon::CBaslerUniversalInstantCamera * camera;
 };
 
-void gst_pylon_initialize () {
+void
+gst_pylon_initialize ()
+{
   Pylon::PylonInitialize ();
 }
 
-GstPylon *gst_pylon_new () {
-  GstPylon *self = (GstPylon*)g_malloc (sizeof (GstPylon));
+GstPylon *
+gst_pylon_new ()
+{
+  GstPylon *self = (GstPylon *) g_malloc (sizeof (GstPylon));
 
   try {
-    self->camera = new Pylon::CBaslerUniversalInstantCamera (Pylon::CTlFactory::GetInstance().CreateFirstDevice());
+    self->camera =
+        new Pylon::CBaslerUniversalInstantCamera (Pylon::CTlFactory::
+        GetInstance ().CreateFirstDevice ());
   }
-  catch (const Pylon::GenericException& e) {
-    std::cerr << "The camera object could not be created." << std::endl << e.GetDescription() << std::endl;
+  catch (const Pylon::GenericException & e)
+  {
+    std::cerr << "The camera object could not be created." << std::endl << e.
+        GetDescription () << std::endl;
   }
 
   return self;
 }
 
-void gst_pylon_free (GstPylon *self) {
+void
+gst_pylon_free (GstPylon * self)
+{
   delete self->camera;
   self->camera = nullptr;
 
   g_free (self);
 }
 
-void gst_pylon_start (GstPylon *self) {
+void
+gst_pylon_start (GstPylon * self)
+{
   try {
-    self->camera->Open();
+    self->camera->Open ();
   }
-  catch (const Pylon::GenericException& e) {
-    std::cerr << "The camera could not be opened." << std::endl << e.GetDescription() << std::endl;
+  catch (const Pylon::GenericException & e)
+  {
+    std::cerr << "The camera could not be opened." << std::endl << e.
+        GetDescription () << std::endl;
   }
 }
 
-void gst_pylon_stop (GstPylon *self) {
+void
+gst_pylon_stop (GstPylon * self)
+{
   try {
-    self->camera->Close();
+    self->camera->Close ();
   }
-  catch (const Pylon::GenericException& e) {
-    std::cerr << "The camera could not be closed." << std::endl << e.GetDescription() << std::endl;
+  catch (const Pylon::GenericException & e)
+  {
+    std::cerr << "The camera could not be closed." << std::endl << e.
+        GetDescription () << std::endl;
   }
 }
--- ext/pylon/gstpylon.h
+++ ext/pylon/gstpylon.h
@@ -11,18 +11,15 @@
 
 #include <glib.h>
 
-G_BEGIN_DECLS
-
-typedef struct _GstPylon GstPylon;
+G_BEGIN_DECLS typedef struct _GstPylon GstPylon;
 
 void gst_pylon_initialize ();
 
 GstPylon *gst_pylon_new ();
-void gst_pylon_free (GstPylon *self);
+void gst_pylon_free (GstPylon * self);
 
-void gst_pylon_start (GstPylon *self);
-void gst_pylon_stop (GstPylon *self);
+void gst_pylon_start (GstPylon * self);
+void gst_pylon_stop (GstPylon * self);
 
 G_END_DECLS
-
-#endif  // GSTPYLON_H_
+#endif // GSTPYLON_H_
