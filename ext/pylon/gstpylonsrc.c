e3022dbeac5b101597353edf801c59571eeac45f
Michael Gruner <michael.gruner@ridgerun.com>
2022-05-23 20:49:28 -0600

    Add extra metadata to the device selection properties

--- ext/pylon/gstpylonsrc.c
+++ ext/pylon/gstpylonsrc.c
@@ -143,12 +143,16 @@ gst_pylon_src_class_init (GstPylonSrcClass * klass)
 
   g_object_class_install_property (gobject_class, PROP_DEVICE_NAME,
       g_param_spec_string ("device-name", "Device name",
-          "The name of the device to use", PROP_DEVICE_NAME_DEFAULT,
-          G_PARAM_READWRITE));
+          "The name of the device to use. Has preference over the device index.",
+          PROP_DEVICE_NAME_DEFAULT,
+          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS |
+          GST_PARAM_MUTABLE_READY));
   g_object_class_install_property (gobject_class, PROP_DEVICE_INDEX,
       g_param_spec_int ("device-index", "Device index",
           "The index of the device to use", PROP_DEVICE_INDEX_MIN,
-          PROP_DEVICE_INDEX_MAX, PROP_DEVICE_INDEX_DEFAULT, G_PARAM_READWRITE));
+          PROP_DEVICE_INDEX_MAX, PROP_DEVICE_INDEX_DEFAULT,
+          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS |
+          GST_PARAM_MUTABLE_READY));
 
   base_src_class->get_caps = GST_DEBUG_FUNCPTR (gst_pylon_src_get_caps);
   base_src_class->fixate = GST_DEBUG_FUNCPTR (gst_pylon_src_fixate);
