9c41d6cd216570a8014332767d31abd21f1b2735
Marisol Zeledon <marisol.zeledon@ridgerun.com>
2022-07-07 10:14:52 -0600

    Separate making enum type into a function

--- ext/pylon/gstpylonintrospection.cpp
+++ ext/pylon/gstpylonintrospection.cpp
@@ -53,8 +53,13 @@ static GParamSpec *gst_pylon_make_spec_str(GenApi::INode *node);
 static GParamSpec *gst_pylon_make_spec_selector_str(GenApi::INode *node,
                                                     GenApi::INode *selector,
                                                     guint64 selector_value);
+static GType gst_pylon_make_enum_type(
+    GenApi::INode *node, Pylon::CBaslerUniversalInstantCamera *camera);
 static GParamSpec *gst_pylon_make_spec_enum(
     GenApi::INode *node, Pylon::CBaslerUniversalInstantCamera *camera);
+static GParamSpec *gst_pylon_make_spec_selector_enum(
+    GenApi::INode *node, Pylon::CBaslerUniversalInstantCamera *camera,
+    GenApi::INode *selector, guint64 selector_value);
 static GParamFlags gst_pylon_query_access(GenApi::INode *node);
 
 static GParamFlags gst_pylon_query_access(GenApi::INode *node) {
@@ -169,7 +174,7 @@ static GParamSpec *gst_pylon_make_spec_selector_str(GenApi::INode *node,
       node->GetToolTip(), param.GetValue(), gst_pylon_query_access(node));
 }
 
-static GParamSpec *gst_pylon_make_spec_enum(
+static GType gst_pylon_make_enum_type(
     GenApi::INode *node, Pylon::CBaslerUniversalInstantCamera *camera) {
   /* When registering enums to the GType system, their string pointers
      must remain valid throughout the application lifespan. To achieve this
@@ -177,8 +182,8 @@ static GParamSpec *gst_pylon_make_spec_enum(
   */
   static std::unordered_map<GType, std::vector<GEnumValue>> persistent_values;
 
-  g_return_val_if_fail(node, NULL);
-  g_return_val_if_fail(camera, NULL);
+  g_return_val_if_fail(node, G_TYPE_INVALID);
+  g_return_val_if_fail(camera, G_TYPE_INVALID);
 
   Pylon::CEnumParameter param(node);
 
@@ -216,6 +221,17 @@ static GParamSpec *gst_pylon_make_spec_enum(
 
   g_free(name);
 
+  return type;
+}
+
+static GParamSpec *gst_pylon_make_spec_enum(
+    GenApi::INode *node, Pylon::CBaslerUniversalInstantCamera *camera) {
+  g_return_val_if_fail(node, NULL);
+  g_return_val_if_fail(camera, NULL);
+
+  Pylon::CEnumParameter param(node);
+  GType type = gst_pylon_make_enum_type(node, camera);
+
   return g_param_spec_enum(node->GetName(), node->GetDisplayName(),
                            node->GetToolTip(), type, param.GetIntValue(),
                            gst_pylon_query_access(node));
@@ -224,51 +240,11 @@ static GParamSpec *gst_pylon_make_spec_enum(
 static GParamSpec *gst_pylon_make_spec_selector_enum(
     GenApi::INode *node, Pylon::CBaslerUniversalInstantCamera *camera,
     GenApi::INode *selector, guint64 selector_value) {
-  /* When registering enums to the GType system, their string pointers
-     must remain valid throughout the application lifespan. To achieve this
-     we are saving all found enums into a static hash table
-  */
-  static std::unordered_map<GType, std::vector<GEnumValue>> persistent_values;
-
   g_return_val_if_fail(node, NULL);
   g_return_val_if_fail(camera, NULL);
 
   Pylon::CEnumParameter param(node);
-
-  gchar *full_name =
-      g_strdup_printf("%s_%s", camera->GetDeviceInfo().GetFullName().c_str(),
-                      node->GetName().c_str());
-  gchar *name = GstPylonParamFactory::sanitize_name(full_name);
-  g_free(full_name);
-
-  GType type = g_type_from_name(name);
-
-  if (!type) {
-    std::vector<GEnumValue> enumvalues;
-    GenApi::StringList_t values;
-
-    param.GetSettableValues(values);
-    for (const auto &value_name : values) {
-      auto entry = param.GetEntryByName(value_name);
-      auto value = static_cast<gint>(entry->GetValue());
-      auto tooltip = entry->GetNode()->GetToolTip();
-
-      /* We need a copy of the strings so that they are persistent
-         throughout the application lifespan */
-      GEnumValue ev = {.value = value,
-                       .value_name = g_strdup(value_name.c_str()),
-                       .value_nick = g_strdup(tooltip.c_str())};
-      enumvalues.push_back(ev);
-    }
-
-    GEnumValue sentinel = {0};
-    enumvalues.push_back(sentinel);
-
-    type = g_enum_register_static(name, enumvalues.data());
-    persistent_values.insert({type, std::move(enumvalues)});
-  }
-
-  g_free(name);
+  GType type = gst_pylon_make_enum_type(node, camera);
 
   return gst_pylon_param_spec_selector_enum(
       node, selector, selector_value, node->GetDisplayName(),
