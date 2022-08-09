23b98672f84561f69391ee27921b39c856f03359
Michael Gruner <michael.gruner@ridgerun.com>
2022-08-09 12:31:53 -0600

    Fill in basic emulator capture+display example

--- tests/examples/pylon/pylonsrc.c
+++ tests/examples/pylon/pylonsrc.c
@@ -34,10 +34,75 @@
  * Example showing usage of the pylonsrc element
  */
 
+#include <gst/gst.h>
+#include <glib-unix.h>
+
+#define PYLONSRC_NAME "src"
+
+static gboolean
+sig_handler (GMainLoop * loop)
+{
+  g_return_val_if_fail (loop, FALSE);
+
+  g_print ("Interrupt caught, exiting...\n");
+  g_main_loop_quit (loop);
+
+  return TRUE;
+}
 
 int
 main (int argc, char **argv)
 {
+  GMainLoop *loop = NULL;
+  GstElement *pipe = NULL;
+  GstElement *pylonsrc = NULL;
+  GError *error = NULL;
+  const gchar *desc =
+      "pylonsrc device-serial-number=0815-0000 name=" PYLONSRC_NAME
+      " ! videoconvert ! autovideosink";
+  gint ret = EXIT_FAILURE;
 
-  return 0;
+  /* Make sure we have an emulator running */
+  g_setenv ("PYLON_CAMEMU", "1", TRUE);
+
+  gst_init (&argc, &argv);
+
+  pipe =
+      gst_parse_launch_full (desc, NULL, GST_PARSE_FLAG_FATAL_ERRORS, &error);
+  if (!pipe) {
+    g_printerr ("Unable to create pipeline: %s\n", error->message);
+    g_error_free (error);
+    goto out;
+  }
+
+  pylonsrc = gst_bin_get_by_name (GST_BIN (pipe), PYLONSRC_NAME);
+  if (!pylonsrc) {
+    g_printerr ("No pylonsrc element found\n");
+    goto free_pipe;
+  }
+
+  if (GST_STATE_CHANGE_FAILURE == gst_element_set_state (pipe,
+          GST_STATE_PLAYING)) {
+    g_printerr ("Unable to play pipeline\n");
+    goto free_pylonsrc;
+  }
+
+  loop = g_main_loop_new (NULL, FALSE);
+  g_unix_signal_add (SIGINT, G_SOURCE_FUNC (sig_handler), loop);
+
+  g_main_loop_run (loop);
+
+  gst_element_set_state (pipe, GST_STATE_NULL);
+
+  g_main_loop_unref (loop);
+  ret = EXIT_SUCCESS;
+
+free_pylonsrc:
+  gst_object_unref (pylonsrc);
+
+free_pipe:
+  gst_object_unref (pipe);
+
+out:
+  return ret;
 }
