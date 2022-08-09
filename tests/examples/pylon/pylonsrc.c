53c320e80007bfcccf112d4cf740f8d0ae32379f
Michael Gruner <michael.gruner@ridgerun.com>
2022-08-09 13:34:47 -0600

    Connect a bus callback to react to errors and warnings

--- tests/examples/pylon/pylonsrc.c
+++ tests/examples/pylon/pylonsrc.c
@@ -44,7 +44,7 @@ sig_handler (GMainLoop * loop)
 {
   g_return_val_if_fail (loop, FALSE);
 
-  g_print ("Interrupt caught, exiting...\n");
+  g_print ("Interrupt caught, exiting...");
   g_main_loop_quit (loop);
 
   return TRUE;
@@ -70,16 +70,57 @@ toggle_pattern (GstElement * pylonsrc)
   return TRUE;
 }
 
+static void
+print_error (GstMessage * msg, GError * error, gchar * dbg, const gchar * tag)
+{
+  g_return_if_fail (msg);
+  g_return_if_fail (error);
+  g_return_if_fail (tag);
+
+  g_printerr ("%s from element %s: %s\n", tag, GST_OBJECT_NAME (msg->src),
+      error->message);
+  g_printerr ("Debugging info: %s\n", (dbg) ? dbg : "none");
+  g_error_free (error);
+  g_free (dbg);
+}
+
+static gboolean
+bus_callback (GstBus * bus, GstMessage * msg, GMainLoop * loop)
+{
+  GError *err = NULL;
+  gchar *dbg_info = NULL;
+
+  g_return_val_if_fail (loop, FALSE);
+
+  switch (GST_MESSAGE_TYPE (msg)) {
+    case GST_MESSAGE_ERROR:
+      gst_message_parse_error (msg, &err, &dbg_info);
+      print_error (msg, err, dbg_info, "ERROR");
+      /* Treat errors as fatal and tear down the app */
+      g_main_loop_quit (loop);
+      break;
+    case GST_MESSAGE_WARNING:
+      gst_message_parse_warning (msg, &err, &dbg_info);
+      print_error (msg, err, dbg_info, "WARNING");
+      break;
+    default:
+      break;
+  }
+
+  return TRUE;
+}
+
 int
 main (int argc, char **argv)
 {
   GMainLoop *loop = NULL;
   GstElement *pipe = NULL;
   GstElement *pylonsrc = NULL;
+  GstBus *bus = NULL;
   GError *error = NULL;
   const gchar *desc =
       "pylonsrc device-serial-number=0815-0000 name=" PYLONSRC_NAME
-      " ! videoconvert ! autovideosink";
+      " ! queue ! videoconvert ! autovideosink";
   gint ret = EXIT_FAILURE;
 
   /* Make sure we have an emulator running */
@@ -95,6 +136,16 @@ main (int argc, char **argv)
     goto out;
   }
 
+  loop = g_main_loop_new (NULL, FALSE);
+  g_unix_signal_add (SIGINT, G_SOURCE_FUNC (sig_handler), loop);
+
+  /* Add a bus listener to receive errors, warnings and other notifications
+   * from the pipeline
+   */
+  bus = gst_pipeline_get_bus (GST_PIPELINE (pipe));
+  gst_bus_add_watch (bus, (GstBusFunc) bus_callback, loop);
+  gst_object_unref (bus);
+
   pylonsrc = gst_bin_get_by_name (GST_BIN (pipe), PYLONSRC_NAME);
   if (!pylonsrc) {
     g_printerr ("No pylonsrc element found\n");
@@ -107,17 +158,18 @@ main (int argc, char **argv)
     goto free_pylonsrc;
   }
 
-  loop = g_main_loop_new (NULL, FALSE);
-  g_unix_signal_add (SIGINT, G_SOURCE_FUNC (sig_handler), loop);
-
+  /* Add a periodic callback to change a camera property */
   g_timeout_add_seconds (1, G_SOURCE_FUNC (toggle_pattern), pylonsrc);
 
+  /* Run until an interrupt is received */
   g_main_loop_run (loop);
 
   gst_element_set_state (pipe, GST_STATE_NULL);
 
   g_main_loop_unref (loop);
   ret = EXIT_SUCCESS;
+
+  g_print ("bye!\n");
 
 free_pylonsrc:
   gst_object_unref (pylonsrc);
