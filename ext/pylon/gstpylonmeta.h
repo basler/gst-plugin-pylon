45b7bef2281c9a1fbdb3ba4a5810ca3ce28cf3d3
Marisol Zeledon <marisol.zeledon@ridgerun.com>
2022-11-03 17:28:13 -0600

    Add block_id, offset and timestamp to GstPylonMeta struct

--- ext/pylon/gstpylonmeta.h
+++ ext/pylon/gstpylonmeta.h
@@ -39,12 +39,23 @@ G_BEGIN_DECLS
 
 #define GST_PYLON_META_API_TYPE (gst_pylon_meta_api_get_type())
 #define GST_PYLON_META_INFO  (gst_pylon_meta_get_info())
+typedef struct _GstPylonOffset GstPylonOffset;
 typedef struct _GstPylonMeta GstPylonMeta;
+
+struct _GstPylonOffset
+{
+  guint64 offset_x;
+  guint64 offset_y;
+};
 
 struct _GstPylonMeta
 {
   GstMeta meta;
+
   GstStructure *chunks;
+  guint64 block_id;
+  GstPylonOffset offset;
+  GstClockTime timestamp;
 };
 
 GstPylonMeta *gst_buffer_add_pylon_meta (GstBuffer * buffer);
