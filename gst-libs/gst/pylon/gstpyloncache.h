9c08f9ef09cd8e0714554b38e0436287d42d6f6e
Marisol Zeledon <marisol.zeledon@ridgerun.com>
2022-12-15 17:48:55 -0600

    Remove unnecessary includes

--- gst-libs/gst/pylon/gstpyloncache.h
+++ gst-libs/gst/pylon/gstpyloncache.h
@@ -34,24 +34,7 @@
 #define _GST_PYLON_CACHE_H_
 
 #include <gst/gst.h>
-
-#ifdef _MSC_VER  // MSVC
-#pragma warning(push)
-#pragma warning(disable : 4265)
-#elif __GNUC__  // GCC, CLANG, MinGW
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
-#pragma GCC diagnostic ignored "-Woverloaded-virtual"
-#pragma GCC diagnostic ignored "-Wunused-variable"
-#endif
-
-#include <pylon/PylonIncludes.h>
-
-#ifdef _MSC_VER  // MSVC
-#pragma warning(pop)
-#elif __GNUC__  // GCC, CLANG, MinWG
-#pragma GCC diagnostic pop
-#endif
+#include <string>
 
 class GstPylonCache {
  public:
