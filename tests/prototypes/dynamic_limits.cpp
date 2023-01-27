2eb1acf2739949b5eb75aaf67023e38eb0a1ad11
Thies Moeller <thies.moeller@baslerweb.com>
2023-01-27 13:28:57 +0100

    ignore warning in pylon API

--- tests/prototypes/dynamic_limits.cpp
+++ tests/prototypes/dynamic_limits.cpp
@@ -38,8 +38,12 @@
 #pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
 #pragma GCC diagnostic ignored "-Woverloaded-virtual"
 #pragma GCC diagnostic ignored "-Wunused-variable"
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wunknown-warning-option"
+#endif
 #endif
 
+#include <pylon/BaslerUniversalInstantCamera.h>
 #include <pylon/PylonIncludes.h>
 
 #ifdef _MSC_VER  // MSVC
