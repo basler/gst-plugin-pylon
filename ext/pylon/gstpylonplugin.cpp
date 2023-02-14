96782da3697717dc2f33827d4408995a9f6a2aa1
Marcelo Sanchez <marcelo.sanchez@ridgerun.com>
2023-02-14 11:56:46 -0600

    Show meson project version when inspecting the element when no git is installed

--- ext/pylon/gstpylonplugin.cpp
+++ ext/pylon/gstpylonplugin.cpp
@@ -43,9 +43,9 @@ static gboolean plugin_init(GstPlugin* plugin) {
                               GST_TYPE_PYLON_SRC);
 }
 
-GST_PLUGIN_DEFINE(GST_VERSION_MAJOR, GST_VERSION_MINOR, pylon,
-                  "Basler/Pylon plugin for pylon SDK " PYLON_VERSIONSTRING_MAJOR
-                  "." PYLON_VERSIONSTRING_MINOR "." PYLON_VERSIONSTRING_SUBMINOR
-                  "(" PYLON_VERSIONSTRING_BUILD ")",
-                  plugin_init, VERSION, GST_PACKAGE_LICENSE, GST_PACKAGE_NAME,
-                  GST_PACKAGE_ORIGIN)
+GST_PLUGIN_DEFINE (GST_VERSION_MAJOR, GST_VERSION_MINOR,
+    pylon,
+    "Basler/Pylon plugin for pylon SDK " PYLON_VERSIONSTRING_MAJOR "."
+    PYLON_VERSIONSTRING_MINOR "." PYLON_VERSIONSTRING_SUBMINOR "("
+    PYLON_VERSIONSTRING_BUILD ")", plugin_init, GIT_VERSION,
+    GST_PACKAGE_LICENSE, GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
