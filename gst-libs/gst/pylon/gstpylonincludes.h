dcf85082b0675c5ce887a3186ca0f672434c7e0f
Thies Moeller <thies.moeller@baslerweb.com>
2023-02-14 14:39:43 +0100

    reformatting with cpp-format

--- gst-libs/gst/pylon/gstpylonincludes.h
+++ gst-libs/gst/pylon/gstpylonincludes.h
@@ -2,25 +2,25 @@
 #define GST_PYLON_INCLUDES_H
 
 #ifdef _MSC_VER  // MSVC
-#pragma warning(push)
-#pragma warning(disable : 4265)
+#  pragma warning(push)
+#  pragma warning(disable : 4265)
 #elif __GNUC__  // GCC, CLANG, MinGW
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
-#pragma GCC diagnostic ignored "-Woverloaded-virtual"
-#pragma GCC diagnostic ignored "-Wunused-variable"
-#ifdef __clang__
-#pragma GCC diagnostic ignored "-Wunknown-warning-option"
-#endif
+#  pragma GCC diagnostic push
+#  pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
+#  pragma GCC diagnostic ignored "-Woverloaded-virtual"
+#  pragma GCC diagnostic ignored "-Wunused-variable"
+#  ifdef __clang__
+#    pragma GCC diagnostic ignored "-Wunknown-warning-option"
+#  endif
 #endif
 
 #include <pylon/BaslerUniversalInstantCamera.h>
 #include <pylon/PylonIncludes.h>
 
 #ifdef _MSC_VER  // MSVC
-#pragma warning(pop)
+#  pragma warning(pop)
 #elif __GNUC__  // GCC, CLANG, MinWG
-#pragma GCC diagnostic pop
+#  pragma GCC diagnostic pop
 #endif
 
 #endif
