5de4664513a779f7a4c42ac818f55208d4a43fb8
Thies Moeller <thies.moeller@baslerweb.com>
2023-05-03 13:16:14 +0200

    cache dimension settings in PAUSED state
    Allow to set offsetx/y early in construction.
    Move the validation of offset vs width/height
    to set_configuration

--- gst-libs/gst/pylon/gstpylonobject.h
+++ gst-libs/gst/pylon/gstpylonobject.h
@@ -44,6 +44,26 @@ struct _GstPylonObjectClass {
   GstObjectClass parent_class;
 };
 
+typedef struct {
+  gint width;
+  gint height;
+  gint offsetx;
+  gint offsety;
+} dimension_t;
+
+typedef struct {
+  std::shared_ptr<Pylon::CBaslerUniversalInstantCamera> camera;
+  GenApi::INodeMap* nodemap;
+  gboolean enable_correction;
+  dimension_t dimension_cache;
+} GstPylonObjectPrivate;
+
+typedef struct {
+  const std::string& device_name;
+  GstPylonCache& feature_cache;
+  GenApi::INodeMap& nodemap;
+} GstPylonObjectDeviceMembers;
+
 EXT_PYLONSRC_API GType gst_pylon_object_register(const std::string& device_name,
                                                  GstPylonCache& feature_cache,
                                                  GenApi::INodeMap& nodemap);
@@ -52,8 +72,11 @@ EXT_PYLONSRC_API GObject* gst_pylon_object_new(
     const std::string& device_name, GenApi::INodeMap* nodemap,
     gboolean enable_correction);
 
-void gst_pylon_object_set_pylon_selector(GenApi::INodeMap& nodemap,
-                                         const gchar* selector_name,
-                                         gint64& selector_value);
+EXT_PYLONSRC_API void gst_pylon_object_set_pylon_selector(
+    GenApi::INodeMap& nodemap, const gchar* selector_name,
+    gint64& selector_value);
+
+EXT_PYLONSRC_API gpointer
+gst_pylon_object_get_instance_private(GstPylonObject* self);
 
 #endif
